877083b2940e8ae140922ada1d566884a19b9d98
Katrin Hofstetter <katrin_hofstetter@gmx.de>
2021-10-13 12:35:48 +0200

    no access-control-allow-origin with * in last lambda function

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -129,9 +129,6 @@ module.exports.getAccessToken = async (event) => {
     console.error(err);
     return {
       statusCode: 500,
-      headers: {
-        "Access-Control-Allow-Origin": "*"
-      },
       body: JSON.stringify(err)
     };
   });

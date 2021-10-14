f95c184ec408a36044b90457459a7914516c68f6
Katrin Hofstetter <katrin_hofstetter@gmx.de>
2021-10-14 16:21:38 +0200

    now with template literals for urls

--- src/api.js
+++ src/api.js
@@ -35,7 +35,7 @@ const removeQuery = () => {
 const getToken = async (code) => {
   const encodeCode = encodeURIComponent(code);
   const { access_token } = await fetch(
-    'https://vpdug6jj84.execute-api.eu-central-1.amazonaws.com/dev/api/token' + '/' + encodeCode
+    `https://vpdug6jj84.execute-api.eu-central-1.amazonaws.com/dev/api/token + / + ${encodeCode}`
   )
     .then((res) => {
       return res.json();
@@ -59,7 +59,7 @@ export const getEvents = async () => {
 
   if (token) {
     removeQuery();
-    const url = 'https://vpdug6jj84.execute-api.eu-central-1.amazonaws.com/dev/api/get-events' + '/' + token;
+    const url = `https://vpdug6jj84.execute-api.eu-central-1.amazonaws.com/dev/api/get-events + / + ${token}`;
     const result = await axios.get(url);
     if (result.data) {
       var locations = extractLocations(result.data.events);

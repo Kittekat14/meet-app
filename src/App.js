f7ade46a90b8bd6bf200cc4c3cd73ce18c281638
Katrin Hofstetter <katrin_hofstetter@gmx.de>
2021-10-14 15:36:52 +0200

    typo in import of nprogress

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
+import './nprogress.css';
 import './App.css';
-import './nprogess.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';

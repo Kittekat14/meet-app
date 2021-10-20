cae0cc5f69056d154249db7626f01c9941b6592c
Katrin Hofstetter <katrin_hofstetter@gmx.de>
2021-10-20 14:03:18 +0200

    less test code so that NumberOfEvents get state over props and not own state anymore

--- src/features/specifyNumberOfEvents.test.js
+++ src/features/specifyNumberOfEvents.test.js
@@ -11,34 +11,34 @@ defineFeature(feature, test => {
 test('When user hasn’t specified a number, 32 is the default number', ({ given, when, then }) => {
   
         given('a user hasn’t specified a number of events to show up', () => {
-          
+          // all stays the same
         });
         let AppWrapper;
         when('the user does nothing', () => {
           AppWrapper = mount(<App />);
-
+        })
         then('the default number of shown events is 32', () => {
           expect(AppWrapper.state("numberOfEvents")).toBe(32);
         });
-    });
+});
 
 test('User can change the number of events they want to see', ({ given, when, then }) => {
-    let NumberOfEventsWrapper;
     let AppWrapper;
 
         given('a list of 32 events is shown to the user', () => {
-          NumberOfEventsWrapper = shallow(<NumberOfEvents updateEvents={() => {}} />);
-          expect(NumberOfEventsWrapper.find(".numberInput").prop("value")).toBe(32);
+          AppWrapper = mount(<App />);
+          
         });
 
         when('the user specifies a number', () => {
-          NumberOfEventsWrapper.find(".numberInput").simulate("change", { target: { value: 23 } } );
+          const numberOfEvents = { target: { value: 23 } };
+          AppWrapper.find(".numberInput").simulate("change", numberOfEvents);
         });
 
         then('there will be as many events shown as the user has specified', () => {
-          const eventCounter = NumberOfEventsWrapper.state('eventCounter');
-          expect(NumberOfEventsWrapper.find('.numberInput').prop('value')).toBe(eventCounter);
+          const NumberOfEventsWrapper = AppWrapper.find(NumberOfEvents);
+          AppWrapper.setState({ numberOfEvents: 23 });
+          expect(NumberOfEventsWrapper.prop('eventCounter')).toBe(23);
         });
-    });
-
-})
+});
+});

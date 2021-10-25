6f417ac96c00e6f284d9c49a3565c3b630fe1890
Katrin Hofstetter <katrin_hofstetter@gmx.de>
2021-10-25 09:52:31 +0200

    repair the styling on Welcome page

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -6,37 +6,6 @@ function WelcomeScreen(props) {
 
   return props.showWelcomeScreen ? (
     <div className="WelcomeScreen">
-<<<<<<< Updated upstream
-      <h1>Welcome to the Meet app</h1>
-      <h4>
-        Log in to see upcoming events around the world for full-stack developers
-      </h4>
-      <div className="button-container" align="center">
-        <div className="google-btn">
-          <div className="google-icon-wrapper">
-            <img
-              className="google-icon"
-              src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
-              alt="Google Sign-In"
-            />
-          </div>
-          <button
-            onClick={() => {
-              props.getAccessToken();
-            }}
-            rel="nofollow noopener"
-            className="btn-text"
-          >Sign In with Google
-          </button>
-        </div>
-      </div>
-      <a
-        href="https://kittekat14.github.io/meet-app/Meet-Privacy-Policy.html"
-        rel="nofollow noopener"
-      >
-        Privacy policy
-      </a>
-=======
       <Container fluid>
         <h1>Welcome to the Meet app</h1>
         <h4>
@@ -66,11 +35,8 @@ function WelcomeScreen(props) {
         <a
           href="https://kittekat14.github.io/meet-app/Meet-Privacy-Policy.html"
           rel="nofollow noopener"
-        >
-          Privacy policy
-        </a>
+        >Privacy policy</a>
       </Container>
->>>>>>> Stashed changes
     </div>
   ) : null;
 }

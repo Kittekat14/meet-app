4fa30ce5aab3aa3783b0d57bb2935b6fea0b3f0b
Katrin Hofstetter <katrin_hofstetter@gmx.de>
2021-10-26 20:00:01 +0200

    WelcomeScreen back to load data

--- src/App.js
+++ src/App.js
@@ -105,25 +105,35 @@ class App extends Component {
               }}
             >
               <CartesianGrid />
-              <XAxis type="category" dataKey="city" name="city" />
-              <YAxis type="number" dataKey="number" name="number of events" />
+              <XAxis 
+                type="category" 
+                dataKey="city" 
+                name="city" 
+                />
+              <YAxis 
+                type="number" 
+                dataKey="number" 
+                name="number of events" 
+                />
               <Tooltip cursor={{ strokeDasharray: "3 3" }} />
-
               <Scatter data={this.getData()} fill="#8884d8" />
-              
+
             </ScatterChart>
           </ResponsiveContainer>
         </div>
 
         <EventList events={this.state.events} />
+
         <NumberOfEvents
           numberOfEvents={this.state.numberOfEvents}
           updateEventNumber={(value) => this.updateEventNumber(value)}
         />
-        {/* <WelcomeScreen
+
+        <WelcomeScreen
           showWelcomeScreen={this.state.showWelcomeScreen}
           getAccessToken={() => getAccessToken()}
-        /> */}
+        />
+
       </div>
     );
   }

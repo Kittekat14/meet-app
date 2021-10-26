8d660b096f58eede1f6d4de88c88cd23ca864262
Katrin Hofstetter <katrin_hofstetter@gmx.de>
2021-10-26 09:36:37 +0200

    aiming test coverage of 100%

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -111,5 +111,11 @@ afterEach(() => {
     });
     expect(AppWrapper.state('numberOfEvents')).toEqual(24);
   });
+
+  test("The updateEvents function gets called when updateEventNumber is called", async () => {
+    await AppWrapper.instance().updateEventNumber();
+    const upEv = await AppWrapper.instance().updateEvents();
+    expect(upEv).toHaveBeenCalled();
+  });
   
 })

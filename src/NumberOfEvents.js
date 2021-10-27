5a713fec774696457c9516f0143607fa9e15716b
Katrin Hofstetter <katrin_hofstetter@gmx.de>
2021-10-27 15:13:00 +0200

    back to before

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -5,7 +5,6 @@ class NumberOfEvents extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      numberOfEvents: 32,
       errorText: ''
     }
   }
@@ -16,12 +15,10 @@ handleInputChanged = (event) => {
 
   if (isNaN(value) || value <= 0) {
     return this.setState({
-      numberOfEvents: 32,
       errorText: "Please write a number bigger than 0",
     });
   } else if (value > 32) {
     return this.setState({
-      numberOfEvents: 32,
       errorText: "Please select a number from 1 to 32",
     });
   } else {

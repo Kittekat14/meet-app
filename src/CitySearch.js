f64a302154bbfb1b32ac16a7a8e94e84c8b8fe7d
Katrin Hofstetter <katrin_hofstetter@gmx.de>
2021-10-28 12:55:33 +0200

    showSuggestion falg to true when location found in suggestions

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -29,7 +29,7 @@ class CitySearch extends Component {
       return this.setState({
         query: value,
         suggestions,
-        showSuggestions: false,
+        showSuggestions: true,
         infoText: "",
       });
     }

3b9dceb010333b9061fda6901e86ee379e609d47
Katrin Hofstetter <katrin_hofstetter@gmx.de>
2021-10-29 12:08:19 +0200

    corrections in PieChart

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -1,21 +1,26 @@
-import React, { PureComponent } from "react";
+import React, { useEffect, useState } from "react";
 import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from "recharts";
 
-export default class EventGenre extends PureComponent {
 
-  getData = () => {
-    // methods to get 
-  }
-  render() {
-    return (
-      <ResponsiveContainer width="100%" height="100%">
+const EventGenre = ({ events }) => {
+  const data = [
+    { name: 'Group A', value: 400 },
+    { name: 'Group B', value: 300 },
+    { name: 'Group C', value: 300 },
+    { name: 'Group D', value: 200 },
+  ];
+
+  const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+  
+  return (
+    <div>
+      <ResponsiveContainer height={400}>
         <PieChart width={400} height={400}>
           <Pie
-            data={this.getData()}
-            cx="50%"
-            cy="50%"
+            data={data}
+            cx={200}
+            cy={200}
             labelLine={false}
-            label={renderCustomizedLabel}
             outerRadius={80}
             fill="#8884d8"
             dataKey="value"
@@ -29,6 +34,8 @@ export default class EventGenre extends PureComponent {
           </Pie>
         </PieChart>
       </ResponsiveContainer>
-    );
-  };
-};
+    </div>
+  );
+}
+
+export default EventGenre;
